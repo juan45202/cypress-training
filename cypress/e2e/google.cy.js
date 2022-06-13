e66d4655d711b09eb8c7ee0d3c8f67c318e2bba5
juan45202 <jjrm.son8@gmail.com>
2022-06-13 10:52:45 -0500

    Setup project configuration
    initial project configuration
    
    Cypress installation and first test

---
+++ cypress/e2e/google.cy.js
@@ -0,0 +1,6 @@
+describe("This is my fisrt Cypress test", () => {
+    it("Should be a title", () => {
+        cy.visit("https://www.google.com/");
+        cy.title().should("eq", "Google");
+    });
+});
