7818a4df874a001df729423e622fa8354cf286dc
juan45202 <jjrm.son8@gmail.com>
2022-06-13 17:11:46 -0500

    Setting up TypeScript tests and static code analysis
    Installing typescript and eslin

--- cypress/e2e/google.cy.js
+++
@@ -1,7 +0,0 @@
-describe("This is my fisrt Cypress test", () => {
-    it("Should be a title", () => {
-        cy.visit("https://www.google.com/");
-        cy.title().should("eq", "Google");
-    });
-    
-});
