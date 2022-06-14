588f2550468ba05294050e0a6294f60481045734
juan45202 <jjrm.son8@gmail.com>
2022-06-14 15:35:55 -0500

    CSS Selectors

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
   e2e: {
     "video": false,
     "screenshotOnRunFailure": false,
+    "defaultCommandTimeout": 20000,
+    "responseTimeout": 20000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
