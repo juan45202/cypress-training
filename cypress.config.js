ef632aa1c856fb7af26cda6ec8e36cc62898c1c2
juan45202 <jjrm.son8@gmail.com>
2022-06-22 11:48:43 -0500

    Improving reports - Mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -9,5 +9,16 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    "reporter": "cypress-multi-reporters",
+    "reporterOptions": {
+        "reporterEnabled": "mochawesome",
+        "mochawesomeReporterOptions": {
+            "reportDir": "cypress/reports/mocha",
+            "quite": true,
+            "overwrite": false,
+            "html": false,
+            "json": true
+        }
+    }
   },
 });
